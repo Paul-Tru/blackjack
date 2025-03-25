f38c6690f7a4ea41319afda78a612a672263abbd
Paul Tru <p.truger@proton.me>
2025-03-25 19:29:51 +0100

    feat(ui): restructure game interface and add coin management
    Create a game frame to organize UI elements and improve layout. 
    Add buttons for "Hit" and "Stand" actions, and implement a 
    tabbed view for coin management with percentage options. 
    This enhances user experience by providing clearer 
    interaction points and better organization of game 
    information.

--- main.py
+++ main.py
@@ -8,9 +8,27 @@ logging.basicConfig(
     datefmt="%d-%m-%Y %H:%M:%S"
 )
 
-coins = 1000
+hit_var:bool = False
+
+def setup():
+    curr_num_label.configure(text="Play?")
+
+def start():
+    usr_sum, usr_cards = game.setup()
+    curr_num_label.configure(text=str(usr_sum))
+    usr_cards_label.configure(text=", ".join(map(str, usr_cards)))
 
 def hit(event=None):
+    global hit_var
+    if hit:
+        for widget in coins_frame.winfo_children():
+            if isinstance(widget, ctk.CTkButton):
+                widget.configure(state="disabled")
+    else:
+        for widget in coins_frame.winfo_children():
+            if isinstance(widget, ctk.CTkButton):
+                widget.configure(state="normal")
+
     result = game.hit()
 
     if isinstance(result, tuple):
@@ -20,10 +38,14 @@ def hit(event=None):
     else:
         end(result)
 
+    update_coin_labels()
+    hit_var = True
+
 
 def stand(event=None):
     winner = game.stand()
     end(winner)
+    update_coin_labels()
 
 
 def end(text):
@@ -35,7 +57,7 @@ def end(text):
 
 
 def replay(event=None):
-    global usr_sum, usr_cards
+    global usr_sum, usr_cards, hit_var
     game.reset()
     game.setup()
 
@@ -45,11 +67,40 @@ def replay(event=None):
     curr_num_label.configure(text=str(game.usr_sum))
     usr_cards_label.configure(text=", ".join(map(str, game.usr_cards)))
 
+    update_coin_labels()
+    hit_var = False
 
-def close():
+
+def close(event=None):
     app.destroy()
 
 
+def handle_percentage(input_percentage: float = None):
+    if input_percentage is None:
+        input_percentage = set_percentage_slider.get()
+
+    input_percentage = int(input_percentage)
+    percentage_label.configure(text=f"{input_percentage}%")
+
+    if set_percentage_slider.get() != input_percentage:
+        set_percentage_slider.set(input_percentage)
+
+    game.usr_set_coins = int(game.usr_coins * (input_percentage/100))
+    game.usr_coins = game.usr_coins - game.usr_set_coins
+    update_coin_labels()
+    start()
+
+def update_coin_labels():
+    coins_label.configure(text=f"Coins:\n {game.usr_coins}")
+    set_coins_label.configure(text=f"Set coins:\n {game.usr_set_coins}")
+
+def handle_return(event):
+    if hit_btn.cget("text") == "Hit":
+        hit()
+    else:
+        replay()
+
+# gui
 app = ctk.CTk()
 app.title("Blackjack")
 
@@ -62,69 +113,65 @@ app.grid_rowconfigure(1, weight=1)
 app.grid_rowconfigure(2, weight=1)
 app.grid_rowconfigure(3, weight=1)
 
-fullscreen:bool = False
-shifted:bool = False
+# game frame
+game_frame = ctk.CTkFrame(app)
+game_frame.grid(row=0, column=1, columnspan=3, rowspan=3, sticky="nsew", padx=10, pady=8)
 
-def is_fullscreen(event=None):
-    global fullscreen
-    found_window_state:bool = (app.winfo_width() >= app.winfo_screenwidth() and
-                          app.winfo_height() >= app.winfo_screenheight())
-    if fullscreen is not found_window_state:
-        fullscreen = found_window_state
-        logging.debug(f"Window state changed, fullscreen: {fullscreen}")
+game_frame.grid_rowconfigure(3, weight=1)
 
-        if fullscreen:
-            shift_elements_right()
+curr_num_label = ctk.CTkLabel(game_frame, text=str(game.usr_sum), font=("Arial", 50, "bold"))
+curr_num_label.grid(row=1, column=0, columnspan=3, sticky="n", padx=10, pady=20)
 
-app.bind("<Configure>", is_fullscreen)
+usr_cards_label = ctk.CTkLabel(game_frame, text=", ".join(map(str, game.usr_cards)), font=("Arial", 20))
+usr_cards_label.grid(row=2, column=0, columnspan=3, padx=10, pady=3)
 
-def shift_elements_right():
-    for widget in app.winfo_children():
-        info = widget.grid_info()
-        if "column" in info:
-            widget.grid(column=info["column"] + 1)
+game_frame.grid_rowconfigure(3, weight=1)
 
-def normalize_elements():
-    global shifted
-    if shifted:
-        for widget in app.winfo_children():
-            info = widget.grid_info()
-            if "column" in info:
-                widget.grid(column=info["column"] - 1)
-        shifted = False
+hit_btn = ctk.CTkButton(game_frame, text="Hit", command=hit)
+hit_btn.grid(row=4, column=0, padx=10, pady=10, sticky="sew")
 
+stand_btn = ctk.CTkButton(game_frame, text="Stand", command=stand)
+stand_btn.grid(row=4, column=2, padx=10, pady=10, sticky="sew")
 
-coins_label = ctk.CTkLabel(app, text=f"Coins: {str(coins)}",
-                           font=("Arial", 12))
-coins_label.grid(row=0, column=0,
-                 padx=15, pady=8)
+# set coin frame
+set_coin_frame = ctk.CTkTabview(app)
+set_coin_frame.grid(row=2, column=0, sticky="nwes", padx=10, pady=8)
 
-coins_label = ctk.CTkLabel(app, text=f"Coins: {coins}", font=("Arial", 12))
-coins_label.grid(row=0, column=0, padx=15, pady=8)
+set_coin_frame.add("Percent")
+ten_percent_btn = ctk.CTkButton(set_coin_frame.tab("Percent"), text="10%", command=lambda: handle_percentage(10))
+ten_percent_btn.grid(row=0, column=0, sticky="ew", padx=10, pady=10)
 
-curr_num_label = ctk.CTkLabel(app, text=str(game.usr_sum), font=("Arial", 50, "bold"))
-curr_num_label.grid(row=1, column=0, columnspan=3, sticky="we", padx=15, pady=8)
+twenty_five_percent_btn = ctk.CTkButton(set_coin_frame.tab("Percent"), text="25%", command=lambda: handle_percentage(25))
+twenty_five_percent_btn.grid(row=0, column=1, sticky="ew", padx=10, pady=10)
 
-usr_cards_label = ctk.CTkLabel(app, text=", ".join(map(str, game.usr_cards)), font=("Arial", 20))  # Fix
-usr_cards_label.grid(row=2, column=0, columnspan=3, sticky="we", padx=15, pady=3)
+fifty_percent_btn = ctk.CTkButton(set_coin_frame.tab("Percent"), text="50%", command=lambda: handle_percentage(50))
+fifty_percent_btn.grid(row=1, column=0, sticky="ew", padx=10, pady=10)
 
-hit_btn = ctk.CTkButton(app, text="Hit", command=hit)
-hit_btn.grid(row=3, column=0, padx=15, pady=10)
+seventy_five_percent_btn = ctk.CTkButton(set_coin_frame.tab("Percent"), text="75%", command=lambda: handle_percentage(75))
+seventy_five_percent_btn.grid(row=1, column=1, sticky="ew", padx=10, pady=10)
 
-stand_btn = ctk.CTkButton(app, text="Stand", command=stand)
-stand_btn.grid(row=3, column=2, padx=15, pady=10)
+percentage_label = ctk.CTkLabel(set_coin_frame.tab("Percent"), text="50%", font=("Arial", 15))
+percentage_label.grid(row=3, column=0, columnspan=2, sticky="ew", padx=10, pady=10)
 
-usr_sum, usr_cards = game.setup()
-curr_num_label.configure(text=str(game.usr_sum))
-usr_cards_label.configure(text=", ".join(map(str, game.usr_cards)))
+set_percentage_slider = ctk.CTkSlider(set_coin_frame.tab("Percent"), from_=0, to=100, command=handle_percentage)
+set_percentage_slider.grid(row=2, column=0, columnspan=2, sticky="ew", padx=10, pady=10)
 
-def handle_return(event):
-    if hit_btn.cget("text") == "Hit":
-        hit()
-    else:
-        replay()
+set_coin_frame.add("Coins")
+
+coins_frame = ctk.CTkFrame(app)
+coins_frame.grid(row=0, column=0, sticky="new", padx=10, pady=8)
+coins_label = ctk.CTkLabel(coins_frame, text=f"Coins:\n {game.usr_coins}", font=("Arial", 20))
+coins_label.grid(row=0, column=0, padx=10, pady=8)
+
+set_coins_frame = ctk.CTkFrame(app)
+set_coins_frame.grid(row=1, column=0, sticky="sew", padx=10, pady=8)
+set_coins_label = ctk.CTkLabel(set_coins_frame, text=f"Set coins:\n {game.usr_set_coins}", font=("Arial", 20))
+set_coins_label.grid(row=0, column=0, padx=10, pady=8)
 
 app.bind("<space>", handle_return)
 app.bind("<Return>", stand)
+app.bind("<Escape>", close)
+
+setup()
 
 app.mainloop()
